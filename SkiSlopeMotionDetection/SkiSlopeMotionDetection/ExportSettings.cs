b97d3337e38c46f8e674d49b32ec905ea4d61416
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-04-24 23:27:41 +0200

    Add handlers for video play/pause, add window for export

---
+++ SkiSlopeMotionDetection/SkiSlopeMotionDetection/ExportSettings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiSlopeMotionDetection
+{
+    public class ExportSettings : INotifyPropertyChanged
+    {
+        private bool exportEntireVideo;
+        private bool includeMarking;
+
+        public bool ExportEntireVideo
+        { 
+            get { return exportEntireVideo; } 
+            set { exportEntireVideo = value; NotifyPropertyChanged(); }
+        }
+        public bool ExportSelectedFrame
+        {
+            get { return !exportEntireVideo; }
+            set { exportEntireVideo = !value; NotifyPropertyChanged(); }
+        }
+        public bool IncludeMarking 
+        { 
+            get { return includeMarking; } 
+            set { includeMarking = value; NotifyPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ExportSettings(bool exportEntireVideo = false, bool includeMarking = true)
+        {
+            ExportEntireVideo = exportEntireVideo;
+            IncludeMarking = includeMarking;
+        }
+    }
+}
--- SkiSlopeMotionDetection/SkiSlopeMotionDetection/MainWindow.xaml.cs
+++ SkiSlopeMotionDetection/SkiSlopeMotionDetection/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -23,16 +24,24 @@ namespace SkiSlopeMotionDetection
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region Private variables
+
         private int currentFrameNumber = 0;
         private int totalFrameNumber = 0;
         private float fpsCounter = 0;
         private int countedPeople = 0;
         private bool shouldAdjustVideoRefreshRate = false;
         private bool shouldMarkPeopleInRealTime = false;
+        private bool isVideoPaused = true;
+        private bool isVideoLoaded = false;
+        private bool isVideoEnded = false;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Properties
+
         public int CurrentFrameNumber
-        { 
+        {
             get { return currentFrameNumber; }
             set { currentFrameNumber = value; NotifyPropertyChanged(); }
         }
@@ -52,12 +61,12 @@ namespace SkiSlopeMotionDetection
             set { countedPeople = value; NotifyPropertyChanged(); }
         }
         public bool UseAdjustedRefreshRate
-        { 
-            get { return shouldAdjustVideoRefreshRate; } 
-            set { shouldAdjustVideoRefreshRate = value; NotifyPropertyChanged(); NotifyPropertyChanged("UseOriginalRefreshRate"); } 
+        {
+            get { return shouldAdjustVideoRefreshRate; }
+            set { shouldAdjustVideoRefreshRate = value; NotifyPropertyChanged(); NotifyPropertyChanged("UseOriginalRefreshRate"); }
         }
         public bool UseOriginalRefreshRate
-        { 
+        {
             get { return !shouldAdjustVideoRefreshRate; }
             set { shouldAdjustVideoRefreshRate = !value; NotifyPropertyChanged(); NotifyPropertyChanged("UseAdjustedRefreshRate"); }
         }
@@ -71,15 +80,111 @@ namespace SkiSlopeMotionDetection
             get { return shouldMarkPeopleInRealTime; }
             set { shouldMarkPeopleInRealTime = value; NotifyPropertyChanged(); NotifyPropertyChanged("MarkPeopleOnPausedFrame"); }
         }
+        public Visibility LoadVideoButtonVisibility
+        {
+            get { return isVideoLoaded ? Visibility.Collapsed : Visibility.Visible; }
+        }
+        public string PlayPauseButtonText
+        {
+            get { return isVideoPaused ? "Play" : "Pause"; }
+        }
+        public bool IsVideoLoaded
+        {
+            get { return isVideoLoaded; }
+            set { isVideoLoaded = value; NotifyPropertyChanged("IsVideoLoaded"); }
+        }
 
+        #endregion
+
+        #region Event handlers
+
+        public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        private void VideoControl_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            IsVideoLoaded = true;
+            NotifyPropertyChanged("LoadVideoButtonVisibility");
+        }
+
+        private void VideoControl_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            isVideoEnded = true;
+            isVideoPaused = true;
+            NotifyPropertyChanged("PlayPauseButtonText");
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Video files (*.mp4;*.avi)|*.mp4;*.avi";
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var path = openFileDialog.FileName;
+                videoControl.Source = new Uri(path);
+                videoControl.Play();
+                videoControl.Pause();
+            }
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            var exportWindow = new ExportWindow();
+            exportWindow.Owner = Window.GetWindow(this);
+
+            exportWindow.Show();
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsVideoLoaded)
+                return;
+
+            if (isVideoPaused)
+                PlayVideo(isVideoEnded);
+            else
+                PauseVideo();
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsVideoLoaded)
+                return;
+
+            PlayVideo(true);
+        }
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region Private methods
+
+        private void PauseVideo()
+        {
+            videoControl.Pause();
+            isVideoPaused = true;
+            NotifyPropertyChanged("PlayPauseButtonText");
+        }
+
+        private void PlayVideo(bool fromBeginning = false)
+        {
+            if (fromBeginning)
+                videoControl.Stop();
+
+            videoControl.Play();
+            isVideoPaused = false;
+            NotifyPropertyChanged("PlayPauseButtonText");
+        }
+
+        #endregion
     }
 }
