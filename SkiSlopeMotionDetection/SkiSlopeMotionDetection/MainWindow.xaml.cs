01a837d4c1ed2184094baa42fa8a77ac47dfa986
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-04-24 19:45:16 +0200

    Add bindings to labels and radio buttons

--- SkiSlopeMotionDetection/SkiSlopeMotionDetection/MainWindow.xaml.cs
+++ SkiSlopeMotionDetection/SkiSlopeMotionDetection/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,8 +21,62 @@ namespace SkiSlopeMotionDetection
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private int currentFrameNumber = 0;
+        private int totalFrameNumber = 0;
+        private float fpsCounter = 0;
+        private int countedPeople = 0;
+        private bool shouldAdjustVideoRefreshRate = false;
+        private bool shouldMarkPeopleInRealTime = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int CurrentFrameNumber
+        { 
+            get { return currentFrameNumber; }
+            set { currentFrameNumber = value; NotifyPropertyChanged(); }
+        }
+        public int TotalFrameNumber
+        {
+            get { return totalFrameNumber; }
+            set { totalFrameNumber = value; NotifyPropertyChanged(); }
+        }
+        public float FPScounter
+        {
+            get { return fpsCounter; }
+            set { fpsCounter = value; NotifyPropertyChanged(); }
+        }
+        public int CountedPeople
+        {
+            get { return countedPeople; }
+            set { countedPeople = value; NotifyPropertyChanged(); }
+        }
+        public bool UseAdjustedRefreshRate
+        { 
+            get { return shouldAdjustVideoRefreshRate; } 
+            set { shouldAdjustVideoRefreshRate = value; NotifyPropertyChanged(); NotifyPropertyChanged("UseOriginalRefreshRate"); } 
+        }
+        public bool UseOriginalRefreshRate
+        { 
+            get { return !shouldAdjustVideoRefreshRate; }
+            set { shouldAdjustVideoRefreshRate = !value; NotifyPropertyChanged(); NotifyPropertyChanged("UseAdjustedRefreshRate"); }
+        }
+        public bool MarkPeopleOnPausedFrame
+        {
+            get { return !shouldMarkPeopleInRealTime; }
+            set { shouldMarkPeopleInRealTime = !value; NotifyPropertyChanged(); NotifyPropertyChanged("MarkPeopleOnEachFrame"); }
+        }
+        public bool MarkPeopleOnEachFrame
+        {
+            get { return shouldMarkPeopleInRealTime; }
+            set { shouldMarkPeopleInRealTime = value; NotifyPropertyChanged(); NotifyPropertyChanged("MarkPeopleOnPausedFrame"); }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
