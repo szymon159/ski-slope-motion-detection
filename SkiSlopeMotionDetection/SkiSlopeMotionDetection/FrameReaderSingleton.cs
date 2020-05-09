3d46e50683fae8908e36feb8dd75226fd4c5353a
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-05-09 21:38:12 +0200

    Add underscore in private variables names

--- SkiSlopeMotionDetection/SkiSlopeMotionDetection/FrameReaderSingleton.cs
+++ SkiSlopeMotionDetection/SkiSlopeMotionDetection/FrameReaderSingleton.cs
@@ -7,32 +7,32 @@ namespace SkiSlopeMotionDetection
 {
     public class FrameReaderSingleton
     {
-        private static FrameReaderSingleton instance = null;
-        private static readonly object padlock = new object();
+        private static FrameReaderSingleton _instance = null;
+        private static readonly object _padlock = new object();
         public static FrameReaderSingleton GetInstance(string videoPath = null)
         {
-            lock (padlock)
+            lock (_padlock)
             {
-                if (instance == null || instance.FilePath != videoPath)
-                    instance = new FrameReaderSingleton(videoPath);
+                if (_instance == null || _instance.FilePath != videoPath)
+                    _instance = new FrameReaderSingleton(videoPath);
 
-                return instance;
+                return _instance;
             }
         }
 
-        private VideoFileReader reader;
+        private VideoFileReader _reader;
         private FrameReaderSingleton(string videoPath)
         {
-            if (instance != null && instance.FilePath == videoPath)
+            if (_instance != null && _instance.FilePath == videoPath)
                 throw new NotSupportedException("Instance of FrameReaderSingleton already exists. Use FrameReaderSingleton.GetInstance()");
             else if (videoPath == null)
                 throw new ArgumentNullException("You must provide a path to video file when creating instance of singleton");
 
-            if (reader != null && reader.IsOpen)
-                reader.Close();
+            if (_reader != null && _reader.IsOpen)
+                _reader.Close();
 
-            reader = new VideoFileReader();
-            reader.Open(videoPath);
+            _reader = new VideoFileReader();
+            _reader.Open(videoPath);
 
             UpdateProperties();
             FileIdentifier = Guid.NewGuid();
@@ -40,16 +40,16 @@ namespace SkiSlopeMotionDetection
         }
         private void UpdateProperties()
         {
-            if (reader == null)
+            if (_reader == null)
             {
-                reader = new VideoFileReader();
-                reader.Open(FilePath);
+                _reader = new VideoFileReader();
+                _reader.Open(FilePath);
             }
 
-            FrameWidth = reader.Width;
-            FrameHeight = reader.Height;
-            FrameCount = reader.FrameCount;
-            FrameRate = reader.FrameRate.Value;
+            FrameWidth = _reader.Width;
+            FrameHeight = _reader.Height;
+            FrameCount = _reader.FrameCount;
+            FrameRate = _reader.FrameRate.Value;
         }
 
         public Guid FileIdentifier { get; private set; }
@@ -65,17 +65,17 @@ namespace SkiSlopeMotionDetection
             if (frameIndex > FrameCount || frameIndex < 0)
                 throw new ArgumentOutOfRangeException($"Unable to get frame number: {frameIndex}. File has only {FrameCount} frames");
 
-            if(reader == null)
+            if(_reader == null)
             {
-                reader = new VideoFileReader();
-                reader.Open(FilePath);
+                _reader = new VideoFileReader();
+                _reader.Open(FilePath);
             }
 
             while (true)
             {
                 try
                 {
-                    result = reader.ReadVideoFrame(frameIndex);
+                    result = _reader.ReadVideoFrame(frameIndex);
                     break;
                 }
                 catch
